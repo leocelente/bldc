9a5ec97a6c58a376e9fd9dfae428e7f4933c3d6d
Leonardo Celente <leocelente@gmail.com>
2023-04-22 18:29:08 -0300

    Create board configuration files with new instrumentation

---
+++ hwconf/other/vesc4/hw_410_LACEP.c
@@ -0,0 +1,277 @@
+/*
+        Copyright 2015 Benjamin Vedder	benjamin@vedder.se
+
+        This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "hw.h"
+
+#include "ch.h"
+#include "hal.h"
+#include "pal.h"
+#include "stm32f4xx_conf.h"
+#include "vesc4/hw_410_LACEP.h"
+
+// Variables
+static volatile bool i2c_running = false;
+
+// I2C configuration
+static const I2CConfig i2cfg = {OPMODE_I2C, 100000, STD_DUTY_CYCLE};
+
+void hw_init_gpio(void) {
+  // GPIO clock enable
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+  // LEDs
+  // LED_GREEN
+  palSetPadMode(GPIOC, 4, PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+  // LED_RED
+  palSetPadMode(GPIOC, 5, PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+
+  // RS485
+  // DE
+  palSetPadMode(HW_RS485_DE_PORT, HW_RS485_DE_PIN,
+                PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+  // RE
+  palSetPadMode(HW_RS485_RE_PORT, HW_RS485_RE_PIN,
+                PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+
+  palWritePad(HW_RS485_DE_PORT, HW_RS485_DE_PIN, 0);
+  palWritePad(HW_RS485_RE_PORT, HW_RS485_RE_PIN, 0);
+
+  // GPIOC (ENABLE_GATE)
+  palSetPadMode(GPIOC, 10, PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+  DISABLE_GATE();
+
+  // GPIOB (DCCAL)
+  palSetPadMode(GPIOB, 12, PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+
+  // GPIOA Configuration: Channel 1 to 3 as alternate function push-pull
+  palSetPadMode(GPIOA, 8,
+                PAL_MODE_ALTERNATE(GPIO_AF_TIM1) | PAL_STM32_OSPEED_HIGHEST |
+                    PAL_STM32_PUDR_FLOATING); // PWM_H3
+  palSetPadMode(GPIOA, 9,
+                PAL_MODE_ALTERNATE(GPIO_AF_TIM1) | PAL_STM32_OSPEED_HIGHEST |
+                    PAL_STM32_PUDR_FLOATING); // PWM_H2
+  palSetPadMode(GPIOA, 10,
+                PAL_MODE_ALTERNATE(GPIO_AF_TIM1) | PAL_STM32_OSPEED_HIGHEST |
+                    PAL_STM32_PUDR_FLOATING); // PWM_H1
+
+  palSetPadMode(GPIOB, 13,
+                PAL_MODE_ALTERNATE(GPIO_AF_TIM1) | PAL_STM32_OSPEED_HIGHEST |
+                    PAL_STM32_PUDR_FLOATING); // PWM_L3
+  palSetPadMode(GPIOB, 14,
+                PAL_MODE_ALTERNATE(GPIO_AF_TIM1) | PAL_STM32_OSPEED_HIGHEST |
+                    PAL_STM32_PUDR_FLOATING); // PWM_L2
+  palSetPadMode(GPIOB, 15,
+                PAL_MODE_ALTERNATE(GPIO_AF_TIM1) | PAL_STM32_OSPEED_HIGHEST |
+                    PAL_STM32_PUDR_FLOATING); // PWM_L1
+
+  // Hall sensors
+  palSetPadMode(HW_HALL_GPIO1, HW_HALL_PIN1, PAL_MODE_INPUT_PULLUP);
+  palSetPadMode(HW_HALL_GPIO2, HW_HALL_PIN2, PAL_MODE_INPUT_PULLUP);
+  palSetPadMode(HW_HALL_GPIO3, HW_HALL_PIN3, PAL_MODE_INPUT_PULLUP);
+
+  // Fault pin
+  palSetPadMode(GPIOC, 12, PAL_MODE_INPUT_PULLUP);
+
+  // ADC Pins
+  palSetPadMode(GPIOA, 0, PAL_MODE_INPUT_ANALOG); // SENS3
+  palSetPadMode(GPIOA, 1, PAL_MODE_INPUT_ANALOG); // SENS2
+  palSetPadMode(GPIOA, 2, PAL_MODE_INPUT_ANALOG); // SENS1
+
+  // Optinal ADC Inputs REMOVED
+  // palSetPadMode(GPIOA, 4, PAL_MODE_INPUT_ANALOG); // TX_SDA_NSS rerouted as
+  // SENS palSetPadMode(GPIOA, 5, PAL_MODE_INPUT_ANALOG); // ADC_EXT  rerouted
+  // as SENS palSetPadMode(GPIOA, 6, PAL_MODE_INPUT_ANALOG); // ADC_EXT2
+  // rerouted as SENS
+
+#ifdef USE_HIGH_SIDE_CURRENT_MEASURE
+  palSetPadMode(GPIOA, 7, PAL_MODE_INPUT_ANALOG); // CURR2
+  palSetPadMode(GPIOA, 6, PAL_MODE_INPUT_ANALOG); // CURR1
+#else
+  palSetPadMode(GPIOB, 0, PAL_MODE_INPUT_ANALOG); // CURR2
+  palSetPadMode(GPIOB, 1, PAL_MODE_INPUT_ANALOG); // CURR1
+#endif
+
+  palSetPadMode(GPIOC, 0, PAL_MODE_INPUT_ANALOG); // TEMP_MOTOR
+  palSetPadMode(GPIOA, 3, PAL_MODE_INPUT_ANALOG); // ADC_TEMP
+  palSetPadMode(GPIOC, 2, PAL_MODE_INPUT_ANALOG); // AN_IN
+}
+
+void hw_setup_adc_channels(void) {
+  /**
+   * The ADC configuration in this project works by running a DMA channel on all
+   ADCs. The DMA will populate the samples to the `ADC_Values` global variable.
+   The order they'll appear in the buffer is determined by the Rank set and the
+   ADCx number
+   *
+   */
+
+  // SENS3
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_15Cycles);
+
+  // CURR2
+#ifdef USE_HIGH_SIDE_CURRENT_MEASURE
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_7, 2, ADC_SampleTime_15Cycles);
+#else
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_8, 2, ADC_SampleTime_15Cycles);
+#endif
+
+  // Vrefint
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_Vrefint, 3,
+                           ADC_SampleTime_15Cycles);
+
+  //! TX_SDA_NSS routed as SENS3
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 4, ADC_SampleTime_15Cycles);
+
+  // ADC2 Channels
+
+  // SENS2
+  ADC_RegularChannelConfig(ADC2, ADC_Channel_1, 1, ADC_SampleTime_15Cycles);
+
+  // CURR1
+#ifdef USE_HIGH_SIDE_CURRENT_MEASURE
+  ADC_RegularChannelConfig(ADC2, ADC_Channel_6, 2, ADC_SampleTime_15Cycles);
+#else
+  ADC_RegularChannelConfig(ADC2, ADC_Channel_9, 2, ADC_SampleTime_15Cycles);
+#endif
+
+  //! ADC_EXT2 routed as SENS2
+  ADC_RegularChannelConfig(ADC2, ADC_Channel_1, 3, ADC_SampleTime_15Cycles);
+  //! ADC_EXT routed as SENS1
+  ADC_RegularChannelConfig(ADC2, ADC_Channel_2, 4, ADC_SampleTime_15Cycles);
+
+  // ADC3 Channels
+
+  // SENS1
+  ADC_RegularChannelConfig(ADC3, ADC_Channel_2, 1, ADC_SampleTime_15Cycles);
+
+  // TEMP_MOSFET (ADC_TEMP)
+  ADC_RegularChannelConfig(ADC3, ADC_Channel_3, 2, ADC_SampleTime_15Cycles);
+  // AN_IN
+  ADC_RegularChannelConfig(ADC3, ADC_Channel_12, 3, ADC_SampleTime_15Cycles);
+  // TEMP_MOTOR
+  ADC_RegularChannelConfig(ADC3, ADC_Channel_10, 4, ADC_SampleTime_15Cycles);
+
+  /**
+   * All the previous settings make sense, they align with exactly one pin and
+   * usage. BUT, these InjectedChannels They have higher priority, so current
+   * measurements are more imediate and synchronized to avoid noise. Its is my
+   * hypothesis that they are each configured twice to couple them (sync them)
+   * no matter which triggers the event
+   */
+#ifdef USE_HIGH_SIDE_CURRENT_MEASURE
+  ADC_InjectedChannelConfig(ADC1, ADC_Channel_6, 1, ADC_SampleTime_15Cycles);
+  ADC_InjectedChannelConfig(ADC1, ADC_Channel_7, 2, ADC_SampleTime_15Cycles);
+  ADC_InjectedChannelConfig(ADC2, ADC_Channel_7, 1, ADC_SampleTime_15Cycles);
+  ADC_InjectedChannelConfig(ADC2, ADC_Channel_6, 2, ADC_SampleTime_15Cycles);
+#else
+  // Injected channels
+  ADC_InjectedChannelConfig(ADC1, ADC_Channel_9, 1, ADC_SampleTime_15Cycles);
+  ADC_InjectedChannelConfig(ADC1, ADC_Channel_8, 2, ADC_SampleTime_15Cycles);
+  ADC_InjectedChannelConfig(ADC2, ADC_Channel_8, 1, ADC_SampleTime_15Cycles);
+  ADC_InjectedChannelConfig(ADC2, ADC_Channel_9, 2, ADC_SampleTime_15Cycles);
+#endif
+}
+
+void hw_start_i2c(void) {
+  i2cAcquireBus(&HW_I2C_DEV);
+
+  if (!i2c_running) {
+    palSetPadMode(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN,
+                  PAL_MODE_ALTERNATE(HW_I2C_GPIO_AF) |
+                      PAL_STM32_OTYPE_OPENDRAIN | PAL_STM32_OSPEED_MID1 |
+                      PAL_STM32_PUDR_PULLUP);
+    palSetPadMode(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN,
+                  PAL_MODE_ALTERNATE(HW_I2C_GPIO_AF) |
+                      PAL_STM32_OTYPE_OPENDRAIN | PAL_STM32_OSPEED_MID1 |
+                      PAL_STM32_PUDR_PULLUP);
+
+    i2cStart(&HW_I2C_DEV, &i2cfg);
+    i2c_running = true;
+  }
+
+  i2cReleaseBus(&HW_I2C_DEV);
+}
+
+void hw_stop_i2c(void) {
+  i2cAcquireBus(&HW_I2C_DEV);
+
+  if (i2c_running) {
+    palSetPadMode(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN, PAL_MODE_INPUT);
+    palSetPadMode(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN, PAL_MODE_INPUT);
+
+    i2cStop(&HW_I2C_DEV);
+    i2c_running = false;
+  }
+
+  i2cReleaseBus(&HW_I2C_DEV);
+}
+
+/**
+ * Try to restore the i2c bus
+ */
+void hw_try_restore_i2c(void) {
+  if (i2c_running) {
+    i2cAcquireBus(&HW_I2C_DEV);
+
+    palSetPadMode(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN,
+                  PAL_STM32_OTYPE_OPENDRAIN | PAL_STM32_OSPEED_MID1 |
+                      PAL_STM32_PUDR_PULLUP);
+
+    palSetPadMode(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN,
+                  PAL_STM32_OTYPE_OPENDRAIN | PAL_STM32_OSPEED_MID1 |
+                      PAL_STM32_PUDR_PULLUP);
+
+    palSetPad(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN);
+    palSetPad(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN);
+
+    chThdSleep(1);
+
+    for (int i = 0; i < 16; i++) {
+      palClearPad(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN);
+      chThdSleep(1);
+      palSetPad(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN);
+      chThdSleep(1);
+    }
+
+    // Generate start then stop condition
+    palClearPad(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN);
+    chThdSleep(1);
+    palClearPad(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN);
+    chThdSleep(1);
+    palSetPad(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN);
+    chThdSleep(1);
+    palSetPad(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN);
+
+    palSetPadMode(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN,
+                  PAL_MODE_ALTERNATE(HW_I2C_GPIO_AF) |
+                      PAL_STM32_OTYPE_OPENDRAIN | PAL_STM32_OSPEED_MID1 |
+                      PAL_STM32_PUDR_PULLUP);
+
+    palSetPadMode(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN,
+                  PAL_MODE_ALTERNATE(HW_I2C_GPIO_AF) |
+                      PAL_STM32_OTYPE_OPENDRAIN | PAL_STM32_OSPEED_MID1 |
+                      PAL_STM32_PUDR_PULLUP);
+
+    HW_I2C_DEV.state = I2C_STOP;
+    i2cStart(&HW_I2C_DEV, &i2cfg);
+
+    i2cReleaseBus(&HW_I2C_DEV);
+  }
+}
---
+++ hwconf/other/vesc4/hw_410_LACEP.h
@@ -0,0 +1,261 @@
+/*
+        Copyright 2015 Benjamin Vedder	benjamin@vedder.se
+
+        This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+
+#ifndef HW_410_LACEP_H_
+#define HW_410_LACEP_H_
+
+#define HW_NAME "410_LACEP"
+#define HW_MAJOR 4
+#define HW_MINOR 10
+#define DISABLE_HW_LIMITS
+// Macros
+#define ENABLE_GATE() palSetPad(GPIOC, 10)
+#define DISABLE_GATE() palClearPad(GPIOC, 10)
+#define DCCAL_ON() palSetPad(GPIOB, 12)
+#define DCCAL_OFF() palClearPad(GPIOB, 12)
+#define IS_DRV_FAULT() (!palReadPad(GPIOC, 12))
+
+#define LED_GREEN_ON() palSetPad(GPIOC, 4)
+#define LED_GREEN_OFF() palClearPad(GPIOC, 4)
+#define LED_RED_ON() palSetPad(GPIOC, 5)
+#define LED_RED_OFF() palClearPad(GPIOC, 5)
+
+/*
+ * ADC Vector
+ *
+ * 0:	ADC1_IN0		  SENS3       (rank 1)
+ * 1:	ADC2_IN1		  SENS2       (rank 1)
+ * 2:	ADC3_IN2		  SENS1       (rank 1)
+ * 3:	ADC1/2_IN8 		CURR2       (rank 2) or ADC1/2_IN7
+ * 4:	ADC1/2_IN9		CURR1       (rank 2) or ADC1/2_IN6
+ * 5:	ADC3_IN3		  TEMP_MOSFET (rank 2)
+ * 6:	ADC1_Vrefint  -           (rank 3)
+ * 7:	ADC2_IN1		  SENS2       (rank 3)
+ * 8:	ADC3_IN12	    AN_IN       (rank 3)
+ * 9:	ADC1_IN0		  SENS3       (rank 4)
+ * 10:ADC2_IN2      SENS1       (rank 4)
+ * 11:ADC3_IN10 	  TEMP_MOTOR  (rank 4)
+ */
+
+#define HW_ADC_CHANNELS 12
+#define HW_ADC_INJ_CHANNELS 2
+/*
+Specifies the number of ADC conversions that will be done using the sequencer
+for regular channel group.
+
+I'm interpreting this as such that the this should be the highest rank number
+used to setup the ADCs. For example if this were set 3 then
+TEMP_MOTOR that is set to the rank 4 would not be measured
+*/
+#define HW_ADC_NBR_CONV 4
+
+/**
+ * @brief Change the ADC channels used. Since this board
+ supports both low side (from driver) and high side (from instrumentation
+ amplifiers)
+ *
+ */
+// #define USE_HIGH_SIDE_CURRENT_MEASURE
+
+// ADC Indexes
+#define ADC_IND_SENS1 2
+#define ADC_IND_SENS2 1
+#define ADC_IND_SENS3 0
+#define ADC_IND_CURR1 4
+#define ADC_IND_CURR2 3
+
+// ADC Indexes
+#define ADC_IND_VIN_SENS 8
+#define ADC_IND_EXT 10
+#define ADC_IND_EXT2 7
+#define ADC_IND_TEMP_MOS 5
+#define ADC_IND_TEMP_MOTOR 11
+#define ADC_IND_VREFINT 6
+
+// ADC macros and settings
+
+// Component parameters (can be overridden)
+#ifndef V_REG
+#define V_REG 3.45
+#endif
+#ifndef VIN_R1
+#define VIN_R1 39000.0
+#endif
+#ifndef VIN_R2
+#define VIN_R2 2200.0
+#endif
+// Override gain and shunt resistance
+#define USE_HIGH_SIDE_CURRENT_MEASURE
+
+#ifdef USE_HIGH_SIDE_CURRENT_MEASURE
+
+// 10.6
+#define CURRENT_AMP_GAIN (1.0f + (49.4e3f / 5.1e3f)) 
+#define CURRENT_SHUNT_RES 0.008f
+#define CURRENT_V_OFFSET (V_REG / 2.0f)
+/**
+ * @brief GET_CURRENTx Macro returns in ADC counts. NOT Volts, the float seems only to be there to confuse.
+ * The conversion to amps is in the FAC_CURRENT macro, that is defined in the conf_general.h
+ * I tried applying the offset of the voltage reference, but the expected offset in ADC counts of 2048 was 
+ * not showing up correctly. I removed the offset calculations on the macro and changed to use
+ * the Current Offset configuration on the FOC menu. The offset was showing at around 2030.
+ * that cleared the FAULT_CODE_HIGH_OFFSET_CURRENT_SENSOR_x fault.
+ */
+#define GET_CURRENT1()                                                         \
+  ((float)(ADC_Value[ADC_IND_CURR1]))
+//  - (CURRENT_V_OFFSET * (4095.0f / V_REG))
+#define GET_CURRENT2()                                                         \
+  ((float)(ADC_Value[ADC_IND_CURR2]))
+//  - (CURRENT_V_OFFSET * (4095.0f / V_REG))
+#endif
+#ifndef CURRENT_AMP_GAIN
+#define CURRENT_AMP_GAIN 10.0
+#endif
+#ifndef CURRENT_SHUNT_RES
+#define CURRENT_SHUNT_RES 0.001
+#endif
+
+// Input voltage
+#define GET_INPUT_VOLTAGE()                                                    \
+  ((V_REG / 4095.0) * (float)ADC_Value[ADC_IND_VIN_SENS] *                     \
+   ((VIN_R1 + VIN_R2) / VIN_R2))
+
+// Voltage on ADC channel
+#define ADC_VOLTS(ch) ((float)ADC_Value[ch] / 4095.0 * V_REG)
+
+// NTC Termistors
+#define NTC_RES(adc_val) ((4095.0 * 10000.0) / adc_val - 10000.0)
+#define NTC_TEMP(adc_ind)                                                      \
+  (1.0 / ((logf(NTC_RES(ADC_Value[adc_ind]) / 10000.0) / 3434.0) +             \
+          (1.0 / 298.15)) -                                                    \
+   273.15)
+
+#define NTC_RES_MOTOR(adc_val)                                                 \
+  (10000.0 / ((4095.0 / (float)adc_val) - 1.0)) // Motor temp sensor on low side
+#define NTC_TEMP_MOTOR(beta)                                                   \
+  (1.0 / ((logf(NTC_RES_MOTOR(ADC_Value[ADC_IND_TEMP_MOTOR]) / 10000.0) /      \
+           beta) +                                                             \
+          (1.0 / 298.15)) -                                                    \
+   273.15)
+
+// Double samples in beginning and end for positive current measurement.
+// Useful when the shunt sense traces have noise that causes offset.
+#ifndef CURR1_DOUBLE_SAMPLE
+#define CURR1_DOUBLE_SAMPLE 0
+#endif
+#ifndef CURR2_DOUBLE_SAMPLE
+#define CURR2_DOUBLE_SAMPLE 0
+#endif
+
+// UART Peripheral
+#define HW_UART_DEV SD3
+#define HW_UART_GPIO_AF GPIO_AF_USART3
+#define HW_UART_TX_PORT GPIOB
+#define HW_UART_TX_PIN 10
+#define HW_UART_RX_PORT GPIOB
+#define HW_UART_RX_PIN 11
+// RS485 RE/DE pins
+#define HW_RS485_DE_PORT GPIOC
+#define HW_RS485_DE_PIN 1
+#define HW_RS485_RE_PORT GPIOC
+#define HW_RS485_RE_PIN 3
+
+// ICU Peripheral for servo decoding
+#define HW_ICU_TIMER TIM4
+#define HW_ICU_TIM_CLK_EN() RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE)
+#define HW_ICU_DEV ICUD4
+#define HW_ICU_CHANNEL ICU_CHANNEL_1
+#define HW_ICU_GPIO_AF GPIO_AF_TIM4
+#define HW_ICU_GPIO GPIOB
+#define HW_ICU_PIN 5
+
+// I2C Peripheral
+#define HW_I2C_DEV I2CD2
+#define HW_I2C_GPIO_AF GPIO_AF_I2C2
+#define HW_I2C_SCL_PORT GPIOB
+#define HW_I2C_SCL_PIN 10
+#define HW_I2C_SDA_PORT GPIOB
+#define HW_I2C_SDA_PIN 11
+
+// Taking advantage of existing code.
+#define HW_USE_SERVO_TIM4 // disables IRQ generation for TIM3, lets LLD handle
+                          // them
+
+// Encoder dedicated pins
+#define HW_HALL_ENC_GPIO1 GPIOC
+#define HW_HALL_ENC_PIN1 6
+#define HW_HALL_ENC_GPIO2 GPIOC
+#define HW_HALL_ENC_PIN2 7
+#define HW_HALL_ENC_GPIO3 GPIOC
+#define HW_HALL_ENC_PIN3 8
+#define HW_ENC_TIM TIM3
+#define HW_ENC_TIM_AF GPIO_AF_TIM3
+#define HW_ENC_TIM_CLK_EN() RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE)
+#define HW_ENC_EXTI_PORTSRC EXTI_PortSourceGPIOC
+#define HW_ENC_EXTI_PINSRC EXTI_PinSource8
+#define HW_ENC_EXTI_CH EXTI9_5_IRQn
+#define HW_ENC_EXTI_LINE EXTI_Line8
+#define HW_ENC_EXTI_ISR_VEC EXTI9_5_IRQHandler
+#define HW_ENC_TIM_ISR_CH TIM3_IRQn
+#define HW_ENC_TIM_ISR_VEC TIM3_IRQHandler
+
+// Hall Sensors dedicated pins
+#define HW_HALL_GPIO1 GPIOB
+#define HW_HALL_PIN1 6
+#define HW_HALL_GPIO2 GPIOB
+#define HW_HALL_PIN2 7
+#define HW_HALL_GPIO3 GPIOC
+#define HW_HALL_PIN3 11
+
+/**
+ * @brief SPI NSS and SCK pins are used as DAC outputs
+ *
+ */
+// SPI pins
+#define HW_SPI_DEV SPID1
+#define HW_SPI_GPIO_AF GPIO_AF_SPI1
+#define HW_SPI_PORT_NSS GPIOA
+#define HW_SPI_PIN_NSS 4
+#define HW_SPI_PORT_SCK GPIOA
+#define HW_SPI_PIN_SCK 5
+#define HW_SPI_PORT_MOSI GPIOA
+#define HW_SPI_PIN_MOSI 7
+#define HW_SPI_PORT_MISO GPIOA
+#define HW_SPI_PIN_MISO 6
+
+// Measurement macros
+#define ADC_V_L1 ADC_Value[ADC_IND_SENS1]
+#define ADC_V_L2 ADC_Value[ADC_IND_SENS2]
+#define ADC_V_L3 ADC_Value[ADC_IND_SENS3]
+#define ADC_V_ZERO (ADC_Value[ADC_IND_VIN_SENS] / 2)
+
+// Macros
+#define READ_HALL1() palReadPad(HW_HALL_GPIO1, HW_HALL_PIN1)
+#define READ_HALL2() palReadPad(HW_HALL_GPIO2, HW_HALL_PIN2)
+#define READ_HALL3() palReadPad(HW_HALL_GPIO3, HW_HALL_PIN3)
+
+// Setting limits
+#define HW_LIM_CURRENT -100.0, 100.0
+#define HW_LIM_CURRENT_IN -100.0, 100.0
+#define HW_LIM_CURRENT_ABS 0.0, 150.0
+#define HW_LIM_VIN 6.0, 57.0
+#define HW_LIM_ERPM -200e3, 200e3
+#define HW_LIM_DUTY_MIN 0.0, 0.1
+#define HW_LIM_DUTY_MAX 0.0, 0.95
+#define HW_LIM_TEMP_FET -40.0, 110.0
+
+#endif /* HW_410_LACEP_H_ */
