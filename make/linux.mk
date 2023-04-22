cb9c8914d3914d9a3a80ce32fc2f0eb96ac7d748
Leonardo Celente <leocelente@gmail.com>
2023-04-22 16:45:59 -0300

    Specify python version

--- make/linux.mk
+++ make/linux.mk
@@ -11,7 +11,7 @@
 
 
 # Get Python version, separate major/minor/patch, then put into wordlist
-PYTHON_VERSION_=$(wordlist 2,4,$(subst ., ,$(shell python -V 2>&1)))
+PYTHON_VERSION_=$(wordlist 2,4,$(subst ., ,$(shell python3 -V 2>&1)))
 
 # Get major version from aforementioned list
 PYTHON_MAJOR_VERSION_=$(word 1,$(PYTHON_VERSION_))
